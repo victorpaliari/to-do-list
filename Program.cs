a0f6cecd7e92310c7fb5b6fd5523de92c3bbf10f
Victor Paliari <victorrpaliari@gmail.com>
2023-10-07 21:25:22 -0300

    Create and Config Database

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using todolist.Data;
+
 namespace todolist
 {
     public class Program
@@ -10,6 +13,16 @@ namespace todolist
             // Add services to the container.
 
             builder.Services.AddControllers();
+
+            // Conexão com o Banco de dados
+            var connectionString = builder.Configuration.
+                    GetConnectionString("DefaultConnection");
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(connectionString)
+            );
+
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
@@ -26,6 +39,15 @@ namespace todolist
             });
 
             var app = builder.Build();
+
+            // Criar o Banco de dados e as tabelas Automaticamente
+            using (var scope = app.Services.CreateAsyncScope())
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                dbContext.Database.EnsureCreated();
+            }
+
+            app.UseDeveloperExceptionPage();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
